7d443427dddd9a7bd1341d76289daafad0b69c51
miguelbaumann1981 <miguelmartinezm@outlook.es>
2026-07-15 13:19:58 +0200

    working on layout

--- src/app/features/public/pages/home-page/home-page.ts
+++ src/app/features/public/pages/home-page/home-page.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ArticleHomeCard } from '../../components/article-home-card/article-home-card';
 
 @Component({
   selector: 'out-home-page',
-  imports: [],
+  imports: [ArticleHomeCard],
   templateUrl: './home-page.html',
 })
-export class HomePage {}
+export class HomePage {
+  isShown = signal(true);
+}
