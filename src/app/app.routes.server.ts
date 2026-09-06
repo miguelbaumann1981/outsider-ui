159e82466563ad8791b090f6f159a90d17562f05
miguelbaumann1981 <miguelmartinezm@outlook.es>
2026-09-06 19:52:08 +0200

    prerender routes app.config.server.ts

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,19 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'admin/releases-crud/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'release/:release',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'articles/:release/:category/:slug',
+    renderMode: RenderMode.Server,
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
