c7400c9bc588795565eb8e6d9c81de9eb2fea7a6
miguelbaumann1981 <miguelmartinezm@outlook.es>
2026-09-06 19:20:54 +0200

    working on environments active as false

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   // API_URL: 'http://localhost:3000',
   API_URL: 'https://outsider-api-node-production.up.railway.app',
-  APP_IS_ACTIVE: true,
+  APP_IS_ACTIVE: false,
 };
