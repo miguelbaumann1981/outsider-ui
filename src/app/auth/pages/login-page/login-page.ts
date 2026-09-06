c2a83f1ce03effdd6d7318b58d4a5382b233bcc8
miguelbaumann1981 <miguelmartinezm@outlook.es>
2026-09-06 20:54:45 +0200

    develop mode

--- src/app/auth/pages/login-page/login-page.ts
+++ src/app/auth/pages/login-page/login-page.ts
@@ -27,8 +27,8 @@ export class LoginPage {
   showPassword = signal(false);
 
   loginModel = signal<LoginFormModel>({
-    email: '',
-    password: '',
+    email: 'baumann@test.com',
+    password: 'Aa1@11',
   });
 
   loginSchema = schema<LoginFormModel>((path) => {
