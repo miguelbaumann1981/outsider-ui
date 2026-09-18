b87b1daadc2d0796decd05449b8995377f891a94
miguelbaumann1981 <miguelmartinezm@outlook.es>
2026-09-18 17:53:02 +0200

    remove credentials

--- src/app/auth/pages/login-page/login-page.ts
+++ src/app/auth/pages/login-page/login-page.ts
@@ -27,8 +27,8 @@ export class LoginPage {
   showPassword = signal(false);
 
   loginModel = signal<LoginFormModel>({
-    email: 'baumann@test.com',
-    password: 'Aa1@11',
+    email: '',
+    password: '',
   });
 
   loginSchema = schema<LoginFormModel>((path) => {
